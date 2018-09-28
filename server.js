12247ea827ea9b8729b7163c580b716466994850
Julien Vernet <julien@vernet.me>
2018-09-28 17:09:05 +0700

    switch to front-end scaping with puppeteer and headless chromium

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var compression = require('compression');
-var express = require('express');
-var TrackService = require('thailand-post').TrackService;
-var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+const compression = require('compression');
+const express = require('express')
+const TrackService = require('thailand-post').TrackService;
+const app = express()
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer')
+const path = require('path');
 
 // compress all responses
 app.use(compression());
@@ -14,11 +15,6 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
-
-// Set language
-var trackService = new TrackService({
-	lang: "EN"
-});
 
 // Show HTML template
 app.get('/', function (req, res) {
@@ -36,23 +32,73 @@ router.use(function (req, res, next) {
 	next();
 });
 
-router.route('/:code').get(function (req, res) {
-	// Get tracking code from URL
-	var trackCode = req.params.code;
-	trackService.init(function (err, serv) {
-		serv.getItem(trackCode, function (err, result) {
-			var lastItem = result.ItemsData.Items.pop();
-			// Create JSON endpoint to be displayed by vue.js
-			var obj = {
-				trackCode: trackCode,
-				title: 'Success! Your item is located at ' + lastItem.Location,
-				items: result.ItemsData.Items,
-				lastItem: lastItem
-			}
-			res.status(200).json(obj);
+router.route('/:emsid').get(function (req, res) {
+	var emsid = req.params.emsid;
+	console.log(" Checking emsid: " + emsid);
+	(async () => {
+		const browser = await puppeteer.launch({
+			headless: true
+		}); // default is true (not show chrome)
+		const page = await browser.newPage();
+		await page.goto('http://track.thailandpost.co.th/tracking/default.aspx?lang=en', {
+			"waitUntil": "networkidle2"
 		});
-	});
-});
+
+		// enter EMS code
+		await page.type('#TextBarcode', emsid);
+
+		// move the slider
+		const e = await page.$('.bgSlider');
+		const box = await e.boundingBox();
+		await page.mouse.move(box.x + 5, box.y + 5);
+		await page.mouse.down();
+		await page.mouse.move(box.x + box.width, box.y + 5); // slide
+		await page.mouse.up();
+
+		// wait for page to reload
+		await page.waitForNavigation();
+
+		// Read HTMl table to array
+		const data = await page.evaluate(() => {
+			const tds = Array.from(document.querySelectorAll(
+				"table:nth-child(3)"
+			))
+			return tds.map(td => {
+				var txt = td.innerText.replace(/<a [^>]+>[^<]*<\/a>/g, '').trim();
+				return txt;
+			});
+		});
+
+		// Modify output format
+		data[1] = data[1].replace(/\t/g, ' ');
+		var tracking = data[1].split("\n");
+		tracking.shift(); // remove วันที่ / เวลา หน่วยงาน  คำอธิบาย  ผลการนำจ่าย
+		var status = [];
+
+		// combine 2 element(date and status) into 1 element
+		for (var i = 0; i + 1 <= tracking.length; i += 2) {
+			temp = tracking[i + 1].split(" "); // 18:02:14 น. สำเหร่  รับเข้าระบบ   
+			temp[3] = "=> " + temp[3].trim(); // change สำเหร่  รับเข้าระบบ to  สำเหร่ => รับเข้าระบบ
+			tracking[i + 1] = temp.join(" ");
+			status.push(tracking[i] + tracking[i + 1]);
+
+			// Create object that will be used by vue.js
+			var obj = {
+				date: tracking[i],
+				time: '',
+				location: '',
+				description: ''
+			}
+			status.push(obj);
+		}
+
+		// status = JSON.stringify(status);
+		// console.log(" " + status);
+		console.log(status);
+		res.status(200).json(status);
+		await browser.close();
+	})();
+})
 
 // All of our routes will be prefixed with /api
 app.use('/api', router);
